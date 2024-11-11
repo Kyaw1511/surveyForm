36348bb739728636200bb2779abe61751f7d200f
Kyaw1511 <kyawminthein151193@gmail.com>
2024-11-11 23:44:14 +0700

    input password

---
+++ script.js
@@ -0,0 +1,20 @@
+// survey form of javascript
+
+const password = document.getElementById("password");
+const lengthCriteria = /.{8,}/;
+const uppercaseCriteria = /[A-Z]/;
+const lowercaseCriteria = /[a-z]/;
+const digitOrSpecialcriteria = /(\d|\W)/;
+
+// define checkPassword function
+function checkPassword() {
+    if(lengthCriteria.test(password) &&
+        uppercaseCriteria.test(password) &&
+        lowercaseCriteria.test(password) &&
+        digitOrSpecialcriteria.test(password)) {
+            console.log("hello world")
+    } else {
+        alert("hi");
+    }
+
+}
